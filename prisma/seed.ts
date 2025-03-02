d29051854fea5e1d2f0db68ba3b3108d776f3e67
Alex <alexander.boev.dev@gmail.com>
2025-03-03 01:32:14 +0300

    feat: up

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,13 +4,16 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash("admin", 10);
+  const hashedPassword = await bcrypt.hash(
+    "~1i1vmlwnpfnQX?r#Bg0|7~}hfaa4yfSHyiOOLbjsvbKZ4Xjcm9b9bN}lzG7jFW@a",
+    10
+  );
 
   await prisma.user.upsert({
-    where: { email: "admin@asb-studio.ru" },
+    where: { email: "alexander@asb-studio.ru" },
     update: {},
     create: {
-      email: "admin@asb-studio.ru",
+      email: "alexander@asb-studio.ru",
       password: hashedPassword,
       role: "ADMIN"
     }
